133416e9e1ddb598321ae60d02a1a46245ad9b89
Paulo77Dev <paulomadson16@gmail.com>
2022-10-14 23:33:39 -0300

    controllers

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal']);
+Route::get('/', [\App\Http\Controllers\SobreNosController::class, 'sobreNos']);
+Route::get('/', [\App\Http\Controllers\ContatoController::class, 'contato']);
 
 
