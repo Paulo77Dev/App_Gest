4f2e51a4801d954007b81046d927efd56c690852
Paulo77Dev <paulomadson16@gmail.com>
2022-10-18 14:24:12 -0300

    editando as views e inserindo o menu de navegação

--- resources/views/site/contato.blade.php
+++ resources/views/site/contato.blade.php
@@ -1,1 +1,15 @@
 <h3>Contato</h3>
+<ul>
+        <li>
+       <a href="/"> Principal</a>
+        </li>
+
+        <li>
+       <a href="/contato"> Contatos</a>
+        </li>
+
+        <li>
+       <a href="/sobre-nos"> Sobre - nósl</a>
+        </li>
+
+</ul>
