552729b151fe817438a2fe100db59c1107163167
Paulo77Dev <paulomadson16@gmail.com>
2022-10-18 14:20:24 -0300

    editando as views e inserindo o menu de navegação

--- resources/views/site/principal.blade.php
+++ resources/views/site/principal.blade.php
@@ -1,1 +1,16 @@
 <h3>Principal</h3>
+
+<ul>
+        <li>
+       <a href="/"> Principal</a>
+        </li>
+
+        <li>
+       <a href="/contato"> Contatos</a>
+        </li>
+
+        <li>
+       <a href="/sobre-nos"> Sobre - nósl</a>
+        </li>
+
+</ul>
