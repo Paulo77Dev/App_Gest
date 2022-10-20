73c49bfc26e6f12513d1728f4d7af9657ecf3e2f
Paulo77Dev <paulomadson16@gmail.com>
2022-10-20 19:56:56 -0300

    alteraçoes

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -71,7 +71,22 @@ Status: {{ $fornecedores[0]['status'] }}
     @endisset
 @endisset
 --}}
+{{--
+@endphp
 
+@isset($fornecedores)
+    Fornecedor: {{ $fornecedores[0]['nome'] }}
+    <br>
+    Status: {{ $fornecedores[0]['status'] }}
+    <br>
+    @isset($fornecedores[0]['cnpj'])
+        CNPJ: {{ $fornecedores[0]['cnpj'] }}
+        @empty($fornecedores[0]['cnpj'])
+            - Vazio
+        @endempty
+    @endisset
+@endisset
+--}}
 
 @endphp
 
