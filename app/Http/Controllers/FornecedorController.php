d910e1e6989d4f2db715ac741337ec3281b35696
Paulo77Dev <paulomadson16@gmail.com>
2022-10-20 20:11:59 -0300

    alteraçoes fornecedores

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -114,15 +114,6 @@ class FornecedorController extends Controller
     }
 }
 */
-<?php
-
-namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
-
-class FornecedorController extends Controller
-{
-    public function index() {
         $fornecedores = [
             0 => [
                 'nome' => 'Fornecedor 1',
