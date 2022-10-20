bf7b3e54facae15b9f1fde2a6f29c3bb5acac6b8
Paulo77Dev <paulomadson16@gmail.com>
2022-10-20 19:46:08 -0300

    alteraçoes

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -21,8 +21,14 @@
 
     }
     */
+
+     /*
+    if(!condicao) {} //enquanto executa se o retorno for true
+    */
 @endphp
 {{-- usando IF E ELSE  no blade --}}
+
+{{--
 @if(count($fornecedores) > 0 && count($fornecedores) < 10)
     <h3>Existem alguns fornecedores cadastrados</h3>
 @elseif(count($fornecedores) > 10)
@@ -30,3 +36,16 @@
 @else
     <h3>Ainda não existem fornecedores cadastrados</h3>
 @endif
+--}}
+Fornecedor: {{ $fornecedores[0]['nome'] }}
+<br>
+Status: {{ $fornecedores[0]['status'] }}
+<br>
+@if( !($fornecedores[0]['status'] == 'S') )
+    Fornecedor inativo
+@endif
+<br>
+@unless($fornecedores[0]['status'] == 'S') <!-- se o retorno da condição for false -->
+    Fornecedor inativo
+@endunless
+<br>
