bfc8924f78bf1d92d170be8edd2780c669f17964
Paulo77Dev <paulomadson16@gmail.com>
2022-10-20 20:06:41 -0300

    alteraçoes

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -67,7 +67,7 @@ class FornecedorController extends Controller
         $msg = isset($fornecedores[0]['cnpj']) ? 'CNPJ informado' : 'CNPJ não informado';
         echo $msg;
 ----------------------------------------------------------------------------------------------------------
-*/
+
         $fornecedores = [
             0 => [
                 'nome' => 'Fornecedor 1',
@@ -78,6 +78,35 @@ class FornecedorController extends Controller
                 'nome' => 'Fornecedor 2',
                 'status' => 'S',
                 'cnpj' => null
+            ]
+        ];
+
+        return view('app.fornecedor.index', compact('fornecedores'));
+    }
+}
+--------------------------------------------------------------------------------------------------------------
+*/
+        $fornecedores = [
+            0 => [
+                'nome' => 'Fornecedor 1',
+                'status' => 'N',
+                'cnpj' => '0',
+                'ddd' => '', //São Paulo (SP)
+                'telefone' => '0000-0000'
+            ],
+            1 => [
+                'nome' => 'Fornecedor 2',
+                'status' => 'S',
+                'cnpj' => null,
+                'ddd' => '85', //Fortaleza (CE)
+                'telefone' => '0000-0000'
+            ],
+            2 => [
+                'nome' => 'Fornecedor 2',
+                'status' => 'S',
+                'cnpj' => null,
+                'ddd' => '32', //Juiz de fora (MG)
+                'telefone' => '0000-0000'
             ]
         ];
 
