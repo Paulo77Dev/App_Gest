b08cdfed4811b901a200431a463e0dc1f40efff1
Paulo77Dev <paulomadson16@gmail.com>
2022-10-20 19:42:38 -0300

    alteraçoes

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -10,6 +10,23 @@
     /*
         Para comentários de multiplas linhas
     */
+   // echo 'Texto de teste';
 
-    echo 'Texto de teste';
+    /*
+    if() {
+
+    } elseif() {
+
+    } else {
+
+    }
+    */
 @endphp
+{{-- usando IF E ELSE  no blade --}}
+@if(count($fornecedores) > 0 && count($fornecedores) < 10)
+    <h3>Existem alguns fornecedores cadastrados</h3>
+@elseif(count($fornecedores) > 10)
+    <h3>Existem vários fornecedores cadastrados</h3>
+@else
+    <h3>Ainda não existem fornecedores cadastrados</h3>
+@endif
