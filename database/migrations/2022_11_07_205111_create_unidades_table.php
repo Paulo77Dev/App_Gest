75de2ca475563e18dbb03061d59314af029e17d8
Paulo77Dev <paulomadson16@gmail.com>
2022-11-07 17:53:36 -0300

    configurando a tabela e fazendo ligação com as unidades

--- database/migrations/2022_11_07_205111_create_unidades_table.php
+++ database/migrations/2022_11_07_205111_create_unidades_table.php
@@ -15,22 +15,22 @@ return new class extends Migration
     {
         Schema::create('unidades', function (Blueprint $table) {
             $table->id();
-            $table->string('unidade', 5); //cm mm kg
+            $table->string('unidade', 5); //cm, mn, kg
             $table->string('descricao', 30);
             $table->timestamps();
         });
-        //adicionar o relacionamento da tabela produtos
-        Schema::create('produtos', function (Blueprint $table) {
-            $table->unsignedBigInteger('unidade_id'); //
+
+        //adicionar o relacionamento com a tabela produtos
+        Schema::table('produtos', function (Blueprint $table) {
+            $table->unsignedBigInteger('unidade_id');
             $table->foreign('unidade_id')->references('id')->on('unidades');
         });
 
-        //adicionar o relacionamento da tabela produtos_detalhes
-        Schema::create('produto_detalhes', function (Blueprint $table) {
-            $table->unsignedBigInteger('unidade_id'); //
+        //adicionar o relacionamento com a tabela produto_detalhes
+        Schema::table('produto_detalhes', function (Blueprint $table) {
+            $table->unsignedBigInteger('unidade_id');
             $table->foreign('unidade_id')->references('id')->on('unidades');
         });
-
     }
 
     /**
@@ -41,11 +41,5 @@ return new class extends Migration
     public function down()
     {
         Schema::dropIfExists('unidades');
-                //adicionar o relacionamento da tabela produtos
-
-
-
-                //adicionar o relacionamento da tabela produtos_detalhes
-
     }
 };
